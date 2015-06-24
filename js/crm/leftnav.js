d7afaa03c74fb8feb5a0b5629c442b53f09356c6
JS-LIU <984003917@qq.com>
2015-06-24 22:13:38 +0800

    test

--- js/crm/leftnav.js
+++ js/crm/leftnav.js
@@ -1,28 +1,28 @@
-//	左侧导航功能
+//  左侧导航功能
 var leftNav = {
-	addModel:{}				//	添加模块
+    addModel:{}             //  添加模块
 };
 
 leftNav.addModel = function(){
-	var $_J_add_model = $('.J-add-model');
-		
-	
-	//	点击添加模块按钮
-	$_J_add_model.click(function(){
-		//	向页面里添加一个大模块
-		addModel();
-	});
-	
-	function addModel(){
-		var $_model = $('.template-box .J-model').clone(true),
-			$_J_model_h = parseInt($('.J-model-h input').val()) || 100;
-		
-		//	模块的高度
-		$_model.css('height',$_J_model_h + 'px');
-		
-		//	插入模块
-		$_model.appendTo($('.main'));
-	};
+    var $_J_add_model = $('.J-add-model');
+        
+    
+    //  点击添加模块按钮
+    $_J_add_model.click(function(){
+        //  向页面里添加一个大模块
+        addModel();
+    });
+    
+    function addModel(){
+        var $_model = $('.template-box .J-model').clone(true),
+            $_J_model_h = parseInt($('.J-model-h input').val()) || 100;
+        
+        //  模块的高度
+        $_model.css('height',$_J_model_h + 'px');
+        
+        //  插入模块
+        $_model.appendTo($('.main'));
+    };
 };
 
 
--- js/crm/model.js
+++ js/crm/model.js
@@ -1,107 +1,139 @@
 var model = {
-	removeModel:{},
-	addchildModel:{},
-	resizeModel:{}
+    removeModel:{},
+    addchildModel:{},
+    resizeModel:{}
 };
 
 model.addchildModel = function(){
-	var smallModelArr = [],
-		$_addSmallModelTopBtn = $('.J-btn-addsmodel-tb'),
-		$_addSmallModelLeftBtn = $('.J-btn-addsmodel-lr'),
-		modelIdArr = [];
-	
-	//	垂直添加小模块
-	function verticalAppend($_parent){
-		var $_parentH = parseFloat($_parent.css('height')),
-			$_smallH = $_parentH / 2,
-			$_smallW = parseFloat($_parent.css('width'));
-			
-		for(var i = 0 ;i < 2 ; i++){
-			var $_smallModel = $('.template-box .J-model').clone(true);
-			$_smallModel.addClass('small-model-v w');
-			
-			$_smallModel.css({
-				height:$_smallH + 'px',
-				width:$_smallW + 'px',
-				margin:'-1px 0px 0px 0px'
-			});
-			smallModelArr.push($_smallModel);
-			addmodelId($_smallModel);
-			$_smallModel.appendTo($_parent);
-		}
-		
-		$_parent.children('.J-btn-addsmodel-tb').hide();
-		$_parent.children('.J-btn-addsmodel-lr').hide();
-		return smallModelArr;	
-	}
-	
-	//	水平添加小模块
-	function horizontalAppend($_parent){
-		var smallModelArr = [],
-			$_parentW = parseFloat($_parent.css('width')),
-			$_parentH = parseFloat($_parent.css('height')),
-			$_smallW = 	$_parentW / 2;
-			
-		$_parent.addClass('clearfix');
-		
-		for(var i = 0 ;i < 2 ; i++){
-			var $_smallModel = $('.template-box .J-model').clone(true);
-			
-			$_smallModel.addClass('small-model-h h fl');
-			
-			$_smallModel.css({
-				width:$_smallW + 'px',
-				height:$_parentH + 'px',
-				margin:'0px 0px 0px -1px'
-			});
-			smallModelArr.push($_smallModel);
-			
-			
-			addmodelId($_smallModel);
-			$_smallModel.appendTo($_parent);
-		}
-		
-		$_parent.children('.J-btn-addsmodel-tb').hide();
-		$_parent.children('.J-btn-addsmodel-lr').hide();
-		return smallModelArr;	
-	}
-	
-	//	为每个小模块编号
-	function addmodelId($_self){
-		var modellen = modelIdArr.length;
-		modelIdArr.push(modellen);
-		$_self.attr('modelId',modellen);
-	};	
-	
-	//	添加最大邻居
-	function addNeighbour(){
-		
-	};
-	
-	$_addSmallModelTopBtn.click(function(){
-		var $_self = $(this).parent();
-		verticalAppend($_self);
-		
-	});
-	
-	
-	$_addSmallModelLeftBtn.click(function(){
-		var $_self = $(this).parent();
-		
-		var smallModel = horizontalAppend($_self);
-		smallModel.map(addNeighbour);
-	});
-	
+    var $_addSmallModelTopBtn = $('.J-btn-addsmodel-tb'),
+        $_addSmallModelLeftBtn = $('.J-btn-addsmodel-lr'),
+        modelIdArr = [];
+        
+    
+    //  垂直添加小模块
+    function verticalAppend($_parent){
+        var smallModelArr = [],
+            $_parentH = parseFloat($_parent.css('height')),
+            $_smallH = $_parentH / 2,
+            $_smallW = parseFloat($_parent.css('width'));
+            
+        for(var i = 0 ;i < 2 ; i++){
+            var $_smallModel = $('.template-box .J-model').clone(true);
+            $_smallModel.addClass('small-model-v w');
+            
+            $_smallModel.css({
+                height:$_smallH + 'px',
+                width:$_smallW + 'px',
+                margin:'-1px 0px 0px 0px'
+            });
+            smallModelArr.push($_smallModel);
+            addmodelId($_smallModel);
+            $_smallModel.appendTo($_parent);
+        }
+        
+        $_parent.children('.J-btn-addsmodel-tb').hide();
+        $_parent.children('.J-btn-addsmodel-lr').hide();
+        return $(smallModelArr);    
+    }
+    
+    //  水平添加小模块
+    function horizontalAppend($_parent){
+        var smallModelArr = [],
+            $_parentW = parseFloat($_parent.css('width')),
+            $_parentH = parseFloat($_parent.css('height')),
+            $_smallW =  $_parentW / 2;
+            
+        $_parent.addClass('clearfix');
+        
+        for(var i = 0 ;i < 2 ; i++){
+            var $_smallModel = $('.template-box .J-model').clone(true);
+            
+            $_smallModel.addClass('small-model-h h fl');
+            
+            $_smallModel.css({
+                width:$_smallW + 'px',
+                height:$_parentH + 'px',
+                margin:'0px 0px 0px -1px'
+            });
+            smallModelArr.push($_smallModel);
+            
+            
+            addmodelId($_smallModel);
+            $_smallModel.appendTo($_parent);
+        }
+        
+        $_parent.children('.J-btn-addsmodel-tb').hide();
+        $_parent.children('.J-btn-addsmodel-lr').hide();
+        return $(smallModelArr);    
+    }
+    
+    //  为每个小模块编号
+    function addmodelId($_self){
+        var modellen = modelIdArr.length;
+        modelIdArr.push(modellen);
+        $_self.attr('modelId',modellen);
+    };  
+
+    var storageNeighbourArr = [],                   //  用来存储左右相邻的小模块的数组
+        storageUpdownArr = [];                      //  用来存储上下相邻的小模块的数组
+    
+    
+    $_addSmallModelTopBtn.click(function(){
+        var $_self = $(this).parent(),
+            smallModel = verticalAppend($_self);
+        storageUpdownArr.push(smallModel);
+    });
+    
+    
+
+    $_addSmallModelLeftBtn.click(function(){
+        var $_self = $(this).parent(),
+            smallModel = horizontalAppend($_self);  //  创建小模板
+
+        storageNeighbourArr.push(smallModel);
+    });
+    
+    return {hArr:storageNeighbourArr,vArr:storageUpdownArr};    
 };
+
 model.resizeModel = function(){
-	var  $_elongate	 = $('.J-elongate');
-	
-	$_elongate.click(function(){
-		
-	
-	});
-
-
+    var neighbourObj = model.addchildModel(),
+        $_elongate = $('.J-elongate'),
+        hDoms = neighbourObj.hArr;
+    
+    function hneibi(self){
+        var $_self = self;
+        for(var i = 0 ,len = hDoms.length; i < len; i++){
+            //  转换dom比较 否则context不同
+            if(hDoms[i][0][0] == $_self[0]){
+                return i;
+                break;
+            }
+        }
+    };
+    
+    $_elongate.mousedown(function(){
+        var $_self = $(this).parent();
+        z = hneibi($_self);
+    });
+    console.log(z);
+    /*$_elongate.bind('mousedrag',function(e,dif){   
+        
+        //  判断当前dom的邻居是谁
+        var $_self = $(this).parent();
+        var hDoms = neighbourObj.hArr;
+        var dif = dif;
+        for(var i = 0 ,len = hDoms.length; i < len; i++){
+            //  转换dom比较 否则context不同
+            if(hDoms[i][0][0] == $_self[0]){
+                //  移动
+                $_self.css('width','+=' + dif + 'px');
+                hDoms[i][0].css('width','-='+dif+'px');
+                console.log(1);
+                break;
+            }
+        }
+    });*/
 };
 
 
